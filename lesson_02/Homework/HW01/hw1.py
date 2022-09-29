960637506870763ce3ec660dd93c93289a5867de
Barker38 <auchanin@inbox.ru>
2022-09-29 16:26:33 +0800

    hw2

---
+++ lesson_02/Homework/HW01/hw1.py
@@ -0,0 +1,10 @@
+from unittest import result
+
+
+chislo = int (input('Введите вещественное число: '))
+int = 0
+while i in chislo:
+    # while chislo != 0:
+    int = (chislo % 10) + chislo
+
+    print(int)
